da942c0650c1030a8cd558b738345d92fc17a49d
Rajdeep Malakar <Rajdeep@tgeeks.cf>
2021-06-28 13:18:15 +0530

    Fix Footer.js

--- components/footer.js
+++ components/footer.js
@@ -2,8 +2,8 @@ import Container from './container'
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
-      &cp; 2021 Zype Inc.
+    <footer className="bg-accent-1 border-t border-accent-2" style="text-align: center">
+      © 2021 Zype Inc.
     </footer>
   )
 }
